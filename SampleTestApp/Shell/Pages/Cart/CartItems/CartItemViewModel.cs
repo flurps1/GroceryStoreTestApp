bd2a4919a70b80511fb4f3d8c79112fdeaef37f6
Maxim <u04@asv.me>
2024-12-10 10:47:45 +0300

    refactor step 3

--- SampleTestApp/Shell/Pages/Cart/CartItems/CartItemViewModel.cs
+++ SampleTestApp/Shell/Pages/Cart/CartItems/CartItemViewModel.cs
@@ -7,8 +7,35 @@ public partial class CartItemViewModel : ViewModelBase
 {
     [ObservableProperty] private int _index;
     [ObservableProperty] private string _name;
-    [ObservableProperty] private int _quantity;
-    [ObservableProperty] private bool _isAvailable;
+
+    private int _quantity;
+    public int Quantity
+    {
+        get => _quantity;
+        set
+        {
+            if (SetProperty(ref _quantity, value))
+            {
+                // Уведомляем об изменении связанных свойств
+                OnPropertyChanged(nameof(AvailabilitySymbol));
+                OnPropertyChanged(nameof(IsAvailable));
+            }
+        }
+    }
+
+    private bool _isAvailable;
+    public bool IsAvailable
+    {
+        get => _isAvailable;
+        set
+        {
+            if (SetProperty(ref _isAvailable, value))
+            {
+                // Уведомляем об изменении связанных свойств
+                OnPropertyChanged(nameof(AvailabilitySymbol));
+            }
+        }
+    }
 
     public Symbol AvailabilitySymbol => IsAvailable ? Symbol.Checkmark : Symbol.Cancel;
 
--- SampleTestApp/Shell/Pages/Cart/CartViewModel.cs
+++ SampleTestApp/Shell/Pages/Cart/CartViewModel.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using DynamicData.Binding;
 using Material.Icons;
 
 namespace SampleTestApp;
@@ -18,12 +19,24 @@ public partial class CartViewModel : PageViewModel
         PageName = ApplicationPageNames.Cart;
         _productService = productService;
         CartItems = cartService.CartItems;
-
+        
         CartItems.CollectionChanged += (s, e) =>
         {
+            if (e.NewItems != null)
+            {
+                foreach (CartItemViewModel newItem in e.NewItems)
+                {
+                    newItem.PropertyChanged += (_, _) => _ = UpdateAvailability();
+                }
+            }
             TotalProducts = CartItems.Sum(x => x.Quantity);
             _ = UpdateAvailability();
         };
+
+        foreach (var item in CartItems)
+        {
+            item.PropertyChanged += (_, _) => _ = UpdateAvailability();
+        }
 
         _ = UpdateAvailability();
     }
@@ -31,12 +44,14 @@ public partial class CartViewModel : PageViewModel
     private async Task UpdateAvailability()
     {
         var products = await _productService.GetProductsAsync();
+    
         foreach (var cartItem in CartItems)
         {
             var product = products.FirstOrDefault(p => p.Name == cartItem.Name);
 
             if (product != null)
             {
+                // Здесь обновляем доступность конкретного товара
                 cartItem.IsAvailable = cartItem.Quantity <= product.Quantity;
             }
         }
